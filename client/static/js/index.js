e54ffdaaf3d3225332387992dfb2efefe24a33f6
rythm <jgon@mit.edu>
2024-05-24 18:13:44 -0500

    make blog-side-panel resize dynamic narrow

--- client/static/js/index.js
+++ client/static/js/index.js
@@ -1,23 +1,23 @@
-const topNavs = document.getElementsByClassName('top-nav');
-const topNavLists = document.getElementsByClassName('top-nav--nav-list');
+const closeNavs = document.getElementsByClassName('close-nav');
+const closeNavLists = document.getElementsByClassName('close-nav--nav-list');
 
-function closeTopNavLists(){
-  Array.from(topNavLists).forEach((topNavList) => {
-    topNavList.classList.remove('top-nav--nav-list__show');
+function hideCloseNavLists(){
+  Array.from(closeNavLists).forEach((closeNavList) => {
+    closeNavList.classList.remove('close-nav--nav-list__show');
   });
 }
 
-Array.from(topNavs).forEach((topNav) => {
-  let ham = topNav.getElementsByClassName('top-nav--drop-ham')[0];
-  let topNavList = topNav.getElementsByClassName('top-nav--nav-list')[0];
+Array.from(closeNavs).forEach((closeNav) => {
+  let ham = closeNav.getElementsByClassName('close-nav--drop-ham')[0];
+  let closeNavList = closeNav.getElementsByClassName('close-nav--nav-list')[0];
 
   ham.addEventListener('click', () => {
-    topNavList.classList.toggle('top-nav--nav-list__show');
+    closeNavList.classList.toggle('close-nav--nav-list__show');
   });
 });
 
 document.addEventListener('click', (e) => {
-  if(!e.target.closest('.top-nav')){
-    closeTopNavLists();
+  if(!e.target.closest('.close-nav')){
+    hideCloseNavLists();
   }
 });
