d404a20fadd54a711c0f2c684dfb62510edd9d0b
rythm <jgon@mit.edu>
2024-05-24 18:42:27 -0500

    side-panel--button change '>' to '<' on open

--- client/static/js/index.js
+++ client/static/js/index.js
@@ -1,9 +1,14 @@
 const closeNavs = document.getElementsByClassName('close-nav');
 const closeNavLists = document.getElementsByClassName('close-nav--nav-list');
+const sideHams = document.getElementsByClassName('side-panel--button');
 
 function hideCloseNavLists(){
   Array.from(closeNavLists).forEach((closeNavList) => {
     closeNavList.classList.remove('close-nav--nav-list__show');
+  });
+
+  Array.from(sideHams).forEach((ham) => {
+    ham.textContent = '>';
   });
 }
 
@@ -12,7 +17,15 @@ Array.from(closeNavs).forEach((closeNav) => {
   let closeNavList = closeNav.getElementsByClassName('close-nav--nav-list')[0];
 
   ham.addEventListener('click', () => {
-    closeNavList.classList.toggle('close-nav--nav-list__show');
+    closeNavList.classList.toggle('close-nav--nav-list__show'); 
+
+    if(ham.classList.contains('side-panel--button')){
+      if(closeNavList.classList.contains('close-nav--nav-list__show')){
+        ham.textContent = '<';
+      }else{
+        ham.textContent = '>';
+      }
+    }
   });
 });
 
