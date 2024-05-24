d5a4b0f7daedb6c8598e4c68d9738b841031dd31
rythm <jgon@mit.edu>
2024-05-24 11:15:48 -0500

    top-nav close click outside

--- client/static/js/index.js
+++ client/static/js/index.js
@@ -1,31 +1,23 @@
 const topNavs = document.getElementsByClassName('top-nav');
-const topNavDropItems = document.getElementsByClassName('top-nav__item--has-drop');
-const dropNavs = document.getElementsByClassName('drop-nav');
+const topNavLists = document.getElementsByClassName('top-nav--nav-list');
 
-function closeDropNavs(){
-  Array.from(dropNavs).forEach((dropNav) => {
-    dropNav.classList.remove('top-nav--nav-list__show');
+function closeTopNavLists(){
+  Array.from(topNavLists).forEach((topNavList) => {
+    topNavList.classList.remove('top-nav--nav-list__show');
   });
 }
 
 Array.from(topNavs).forEach((topNav) => {
   let ham = topNav.getElementsByClassName('top-nav--drop-ham')[0];
-  let dropNav = topNav.getElementsByClassName('top-nav--nav-list')[0];
+  let topNavList = topNav.getElementsByClassName('top-nav--nav-list')[0];
 
   ham.addEventListener('click', () => {
-    closeDropNavs();
-
-    dropNav.classList.toggle('top-nav--nav-list__show');
+    topNavList.classList.toggle('top-nav--nav-list__show');
   });
 });
 
-Array.from(topNavDropItems).forEach((navItem) => {
-  let dropButton = navItem.getElementsByClassName('top-nav--drop-ham')[0];
-  let dropNav = navItem.getElementsByClassName('top-nav--nav-list')[0];
-
-  dropButton.addEventListener('click', () => {
-    if(!dropNav.classList.contains('top-nav--nav-list__show')) closeDropNavs();
-
-    dropNav.classList.toggle('top-nav--nav-list__show');
-  });
+document.addEventListener('click', (e) => {
+  if(!e.target.closest('.top-nav')){
+    closeTopNavLists();
+  }
 });
